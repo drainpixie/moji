002a0a1bc6111f49c49c6dd99308132183493bf4
drainpixie <121581793+drainpixie@users.noreply.github.com>
2025-05-26 16:18:31 +0200

    chore: add theme-color meta

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -47,6 +47,7 @@
 		rel="icon"
 		href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎛️</text></svg>"
 	/>
+	<meta name="theme-color" content="#ffffff" />
 </svelte:head>
 
 <main class="flex h-screen flex-col items-center justify-center bg-neutral-50 dark:bg-neutral-950">
