1817276dedcbff926c66041fe6bb74d0ee46edc7
drainpixie <121581793+drainpixie@users.noreply.github.com>
2025-05-26 20:58:25 +0200

    chore: add jsdoc to api

--- src/lib/state.svelte.ts
+++ src/lib/state.svelte.ts
@@ -1,22 +1,53 @@
-// Create state variables internally (not exported)
 let _query = $state('');
 let _isOpen = $state(false);
 let _selectedIndex = $state(0);
 
+/**
+ * Get the query
+ */
 export const query = () => _query;
-export const isOpen = () => {
-	console.log(_isOpen);
-	return _isOpen;
-};
+
+/**
+ * Get the state
+ */
+export const isOpen = () => _isOpen;
+
+/**
+ * Get the selected index
+ */
 export const selectedIndex = () => _selectedIndex;
 
+/**
+ * Set the command bar to open
+ */
 export const open = () => (_isOpen = true);
+
+/**
+ * Set the command bar to closed
+ */
 export const close = () => (_isOpen = false);
-export const toggle = () => (_isOpen = !_isOpen);
+
+/**
+ * Toggle the command bar
+ */
+export const toggle = () => void (_isOpen = !_isOpen);
+
+/**
+ * Clear the query and selected index
+ */
 export const clear = () => {
 	_query = '';
 	_selectedIndex = 0;
 };
 
+/**
+ * Set the selected index
+ * @param index
+ */
 export const setIndex = (index: number) => (_selectedIndex = index);
+
+/**
+ * Set the query
+ * @param input
+ */
 export const setQuery = (input: string) => (_query = input);
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,16 +1,39 @@
 import type { Component } from 'svelte';
 
+/**
+ * The command object to display in the command bar.
+ */
 export interface Command {
+	/**
+	 * The text to display in the command bar.
+	 */
 	text: string;
 
-	// TODO: Add support for hotkey
+	/**
+	 * TODO: Add support for hotkey
+	 * The hotkey to trigger the command.
+	 */
 	hotkey?: (e: KeyboardEvent) => boolean;
 
+	/**
+	 * The icon to display next to the command text.
+	 */
 	icon?: Component;
+
+	/**
+	 * The category to group the command under.
+	 */
 	category: string;
+
+	/**
+	 * The function to trigger when the command is clicked.
+	 */
 	trigger: (...args: unknown[]) => unknown;
 }
 
+/**
+ * The props for the CommandBar component.
+ */
 export interface CommandBarProps {
 	/**
 	 * The commands to display in the command bar.
